942c2539a91a39707c8f2b32cc694282dced3dae
muzhenhua <493688171@qq.com>
2019-11-12 11:04:28 +0800

    第三次提交

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+<<<<<<< c5ae32e643bd96e14ce9a78987770e4a723ce14c
 # loginRSA
+=======
+# loginRSA
+(最标准的公钥加密,私钥解密)后台生成RSA公钥以及私钥，私钥保密，公钥下发，前端登录使用公钥加密。后台登录验证使用私钥解密
+
+![登录效果图](https://github.com/Nj-LiuLiFei/loginRSA/blob/master/src/main/webapp/img/1.png)
+
+![解密效果图](https://github.com/Nj-LiuLiFei/loginRSA/blob/master/src/main/webapp/img/2.png)
+>>>>>>> 首次提交
---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.controller;
+
+
+import com.util.RSACoder;
+import com.utils.JsonUtil;
+import com.utils.RSAUtils;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping(value = "login",method = RequestMethod.POST)
+    @ResponseBody
+    public String login(@RequestParam("password") String password,@RequestParam("uname") String uname ,Model model, HttpServletRequest httpServletRequest){
+
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        try {
+            String PRIVATEKEY = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIn2zWqU7K/2qm5pOpq5bp9R+3MTnStWTfJU9nC/Vo7UKH9dITPvrELCTK+qlqpx5Fes+l0GY7n6u4n4jyiw4ejsvkZYQ5ww477yLOn2FcoEGuZEwPgSCmfTST0OFUgQqn+/J11k9L92jEHyieE3qmhMkMt0UsVUSJwx/nZxo30ZAgMBAAECgYBD3YHigeuEC4R+14iaf8jo2j0kuGtB3Cxvnlez0otTqw1YyYkBsU49cLKkXvfKVEgM0Ow/QltgKvSBxCE31PrrDka5TygVMqqA/IM7NrDvjUcGLjyoeNmLA8660fWcDxUTlAGN5kxIvUATayVwKVflpWPWu0FPKsWrZustnEo+4QJBAMCmYsWqAKWYMVRXFP3/XGRfio8DV793TOckyBSN9eh8UhgoZyT3u7oeHmDJEwm4aNMHlg1Pcdc6tNsvi1FRCiUCQQC3VNzfF4xOtUgX7vWPL8YVljLuXmy12iVYmg6ofu9l31nwM9FLQ1TRFglvF5LWrIXTQb07PgGd5DJMAQWGsqLlAkAPE7Z9M73TN+L8b8hDzJ1leZi1cpSGdoa9PEKwYR/SrxAZtefEm+LEQSEtf+8OfrEtetWCeyo0pvKKiOEFXytFAkEAgynL/DC0yXsZYUYtmYvshHU5ayFTVagFICbYZeSrEo+BoUDxdI9vl0fU6A5NmBlGhaZ65G+waG5jLc1tTrlvoQJAXBEoPcBNAosiZHQfYBwHqU6mJ9/ZacJh3MtJzGGebfEwJgtln5b154iANqNWXpySBLvkK+Boq7FYRiD83pqmUg==";
+            // 解密
+            uname = RSAUtils.decryptDataOnJava(uname, PRIVATEKEY);
+            password = RSAUtils.decryptDataOnJava(password, PRIVATEKEY);
+            // 用map封装返回的数据
+
+            result.put("uname", uname);
+            result.put("password", password);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return JsonUtil.toJson(result);
+    }
+}
